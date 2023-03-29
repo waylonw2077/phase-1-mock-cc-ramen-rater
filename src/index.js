3e9bcb922395efe29f3f5fc746b70f6f8552cfe6
waylon wickline <waylonw2077@LAPTOP-6MQVH55Q>
2023-03-29 10:31:16 -0400

    adds images

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 // write your code here
+const ramenMenu = document.querySelector('#ramen-menu')
+fetch('http:localhost:3000/ramens')
+.then(response => response.json)
+.then((data=> {
+data.forEach(ramen => {
+    const img = document.createElement('img')
+})
+}))
